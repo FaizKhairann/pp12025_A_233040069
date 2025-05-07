a0b983a13d1e5b5e55e0491d97e488edbfafbde9
FaizKhairann <faizkhairan6@gmail.com>
2025-05-07 14:09:47 +0700

    pertemuan 8

--- ModulProject/src/TUGAS/TUGAS HARIAN/Pertemuan7/Tugas/StrukturListTest.java
+++ ModulProject/src/TUGAS/TUGAS HARIAN/Pertemuan7/Tugas/StrukturListTest.java
@@ -2,16 +2,16 @@ package Pertemuan7.Tugas;
 
 public class StrukturListTest {
   public static void main(String[] args) {
-    StrukturList list = new StrukturList();
-
-    list.addTail(5);
-    list.addTail(4);
-    list.addTail(6);
-    list.displayElement();
-    System.out.println(list.find(6)); // true
+    // StrukturList list = new StrukturList();
+    // // Latihan 2
+    // list.addTail(5);
+    // list.addTail(4);
+    // list.addTail(6);
+    // list.displayElement();
+    // System.out.println(list.find(6)); // true
 
     StrukturList list2 = new StrukturList();
-
+    // Latihan 4
     list2.addTail(7);
     list2.addTail(6);
     list2.addTail(4);
@@ -19,5 +19,6 @@ public class StrukturListTest {
     list2.addTail(2);
     list2.displayElement();
     System.out.println("Jumlah elemen: " + list2.size());
+
   }
 }
---
+++ ModulProject/src/TUGAS/TUGAS HARIAN/Pertemuan8/latihan/ListMkTest.java
@@ -0,0 +1,25 @@
+package Pertemuan8.latihan;
+
+public class ListMkTest {
+
+  public static void main(String[] args) {
+    StrukturList list = new StrukturList();
+    // latihan 4
+    // list.addHead(new Matakuliah("IF1001", "Dasar Pemrograman", 4));
+    // list.addHead(new Matakuliah("IF1001", "Pemrograman Web", 3));
+
+    // // tes 1
+    // list.addHead(new Matakuliah("IF001", "Dasar Pemrograman", 4));
+    // list.addHead(new Matakuliah("IF002", "Pemrograman Web", 3));
+    // list.addHead(new Matakuliah("IF003", "Struktur Diskrit", 3));
+    // list.addHead(new Matakuliah("IF004", "konstruksi PL Berorientasi Objek", 3));
+
+    // tes 4
+    list.addTail(new Matakuliah("IF004", "Konstruksi PL Berorientasi Objek", 3));
+    list.addTail(new Matakuliah("IF004", "Struktur Diskrit", 3));
+    list.addTail(new Matakuliah("IF004", "Pemrograman Web", 3));
+    list.addMid(new Matakuliah("IF001", "Dasar Pemrogrman", 4), 4);
+
+    list.displayElement();
+  }
+}
